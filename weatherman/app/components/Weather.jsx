00883e2bffdadccd01c20a25c9f8905df91a1f8d
Anton <antongotfat@gmail.com>
2017-06-09 19:44:19 +0300

    Add loading text

--- weatherman/app/components/Weather.jsx
+++ weatherman/app/components/Weather.jsx
@@ -4,35 +4,46 @@ var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
-  getInitialState: function() {
+  getInitialState: function () {
     return {
-      location: 'Miami',
-      temp: 88
-    };
+      isLoading: false
+    }
   },
-  handelSearch : function(location) {
+  handleSearch: function (location) {
     var that = this;
+
+    this.setState({isLoading: true});
 
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
-        temp: temp
+        temp: temp,
+        isLoading: false
       });
     }, function (errorMessage) {
+      that.setState({isLoading: false});
       alert(errorMessage);
     });
   },
-  render: function() {
+  render: function () {
+    var {isLoading, temp, location} = this.state;
     //var temp = this.state.temp;
     //var location = this.state.location;
-    var {temp, location} = this.state;
-    return(
+    function renderMessage () {
+      if (isLoading) {
+        return <h3>Fetching weather...</h3>;
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+
+    return (
       <div>
         <h3>Weather Component</h3>
-        <WeatherForm onSearch={this.handelSearch}/>
-        <WeatherMessage temp={temp} location={location}/>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
       </div>
-    );
+    )
   }
 });
 
